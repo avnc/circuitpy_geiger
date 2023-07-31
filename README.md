ef208e1987e46a493414d13827ae693a580c0f36
Andrew Vessey <129348919+avnc@users.noreply.github.com>
2023-07-31 12:11:32 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# circuitpy_geiger
-CircuitPython DFRobot Geiger counter implementation
+# Geiger Counter
+## CircuitPython DFRobot Geiger counter implementation.
+
+*NOTE: CANNOT GUARANTEE THIS WORKS, AND CANNOT TEST IT.* To quote DFRobot: **"This product is not a professional measuring instrument, it is only suitable for principle research and teaching demonstration. Not for use in radiation dose measurement that directly affects personal safety."**
+
+This is a simple CircuitPython application that runs on an Adafruit Metro M4 Express (but could be easily modified to run on many others). It uses a DFRobot Geiger Counter sensor and has code to implement simple CPM (counts per minute) and uSv/h metrics which are then reported to Adafruit IO. The DFRobot sensor could easily be wired differently but I was trying to preserve the existing wiring harness so I could try it with a few other Arduino compatibile format boards. As such, I ended up with the following:
+
+- Adafruit Metro M4 Express with Airlift ([Adafruit](https://www.adafruit.com/product/4000))
+- A DFRobot Gravity IO Extension board ([Amazon](https://a.co/d/dOZaMgP) or [DFRobot](https://www.dfrobot.com/product-1009.html))
+- And, of course, the DF Robot Geiger counter module (I got mine from [PiHut](https://thepihut.com/products/gravity-geiger-counter-module-ionizing-radiation-detector) or [DFRobot](https://www.dfrobot.com/product-2547.html))
+
+![image of completed project](https://github.com/avnc/geiger/blob/b3079368eeb564b645382b995fef11e30f33ef3a/IMG_1430.jpeg)
+
+You could skip the IO extension board (and just cut the wires) but was simpler this way. I made a simple enclosure in TinkerCad that I 3D printed to hold the thing - it's awful and I will not share until I can clean it up... 
+
+The geiger tube itself relies on *high voltage*, so best to heed the warnings on the [DFRobot wiki](https://wiki.dfrobot.com/SKU_SEN0463_Gravity_Geiger_Counter_Module):
+
+**"⚡ The Geiger tube is driven by a voltage of up to 400V, after powering on do not touch the high voltage circuit near the positive pole of the Geiger tube."**
+
+I didn't find any other Circuitpython or Micropython code but DFRobot does provide an Arduino library that you can take a look at [here on Github](https://github.com/cdjq/DFRobot_Geiger).
+
+To run the code as provided, you'll need the Metro M4 Express, however it shouldn't be hard to run with other Circuitpython supported microcontrollers. Adafruit IO account also needed but you can [set one up for free here](https://io.adafruit.com/) (or just comment out that part of the code). The setttings.toml file has placeholders for you to fill in your WiFi and Adafruit IO info.
---
+++ code.py
@@ -0,0 +1,85 @@
+from os import getenv
+import board
+import digitalio 
+import time
+from busio import SPI
+import adafruit_esp32spi.adafruit_esp32spi_socket as socket
+from adafruit_esp32spi import adafruit_esp32spi
+from adafruit_io.adafruit_io import IO_HTTP
+from adafruit_io.adafruit_io import AdafruitIO_RequestError
+import adafruit_requests as requests
+
+
+# Using a Metro board with pre-defined ESP32 Pins, so gotta do this to get it all setup (copied from Adafruit example code)
+esp32_cs = digitalio.DigitalInOut(board.ESP_CS)
+esp32_ready = digitalio.DigitalInOut(board.ESP_BUSY)
+esp32_reset = digitalio.DigitalInOut(board.ESP_RESET)
+
+spi = SPI(board.SCK, board.MOSI, board.MISO)
+esp = adafruit_esp32spi.ESP_SPIcontrol(spi, esp32_cs, esp32_ready, esp32_reset)
+
+requests.set_socket(socket, esp)
+
+if esp.status == adafruit_esp32spi.WL_IDLE_STATUS:
+    print("ESP32 found and in idle mode")
+print("Firmware vers.", esp.firmware_version)
+print("MAC addr:", [hex(i) for i in esp.MAC_address])
+
+for ap in esp.scan_networks():
+    print("\t%s\t\tRSSI: %d" % (str(ap["ssid"], "utf-8"), ap["rssi"]))
+
+print("Connecting to AP...")
+while not esp.is_connected:
+    try:
+        esp.connect_AP(getenv('CIRCUITPY_WIFI_SSID'), getenv('CIRCUITPY_WIFI_PASSWORD'))
+    except OSError as e:
+        print("could not connect to AP, retrying: ", e)
+        continue
+print("Connected to", str(esp.ssid, "utf-8"), "\tRSSI:", esp.rssi)
+print("My IP address is", esp.pretty_ip(esp.ip_address))
+
+# Initialize an Adafruit IO HTTP API object
+io = IO_HTTP(getenv('AIO_USERNAME'), getenv('AIO_KEY'), requests)
+
+# check for feeds (and create if not there)
+try:
+    # Get the 'radiation' feeds from Adafruit IO
+    cpm_feed = io.get_feed("radiation.cpm")
+    usv_feed = io.get_feed("radiation.usv-slash-h")
+except AdafruitIO_RequestError:
+    # If no 'radiation' feeds exists, create
+    rad_feed = io.create_new_feed("radiation")
+    usv_feed = io.create_new_feed("radiation.usv-slash-h")
+
+# setup Geiger counter pin
+geiger_pin = digitalio.DigitalInOut(board.D3)  # Use the correct pin for your setup
+geiger_pin.direction = digitalio.Direction.INPUT
+geiger_pin.pull = digitalio.Pull.UP  # If necessary, use a pull-up or pull-down resistor
+
+# to get to uSv/h
+conversion_factor = 151
+count = 0
+previous_state = False
+
+start_time = time.monotonic()
+
+while True:
+    current_state = geiger_pin.value
+    if current_state and not previous_state:
+        # rising edge detected
+        count += 1
+    previous_state = current_state
+    
+    # this code here could be better but works for our half-assed implementation
+    if time.monotonic() - start_time >= 60:
+        # a minute has passed
+        cpm = count
+        micro_sv = round(count/conversion_factor, 2)
+        # reset time and count before we send data as this can take a little time
+        start_time = time.monotonic()
+        count = 0
+        print(f'Counts per minute: {cpm}')
+        print(f'uSv/h is {micro_sv}')
+        # send data to feeds
+        io.send_data(cpm_feed["key"], cpm)
+        io.send_data(usv_feed["key"], micro_sv)
